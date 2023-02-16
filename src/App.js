917e0beb2a2de5dc309f77d48c6f4288201e6cb3
dawei-wang <dawei-wang@users.noreply.github.com>
2023-02-16 15:06:02 -0800

    Add Twinkle Twinkle little star color-coded

--- src/App.js
+++ src/App.js
@@ -1,60 +1,70 @@
-import React, { useState, useRef, useEffect } from "react";
-import * as abcjs from "abcjs";
-import PDFDocument from "jspdf";
-import "./App.css";
-import { saveAs } from "file-saver";
+import React, { useState } from "react";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
+import { renderToSvg } from "abcjs";
 
-const colors = [
-  "#ff0000",
-  "#00ff00",
-  "#0000ff",
-  "#ffff00",
-  "#00ffff",
-  "#ff00ff",
-  "#000000",
-  "#ffffff",
-];
+const COLORS = {
+  1: "#ff6666",
+  2: "#ff9966",
+  3: "#ffff66",
+  4: "#99ff99",
+  5: "#66b3ff",
+  6: "#d966ff",
+};
+
+const song =
+  "c4 c4 g4 g4 a4 a4 g4 e4 e4 d4 d4 c4 g4 g4 f4 f4 e4 e4 d4 g4 g4 f4 f4 e4 e4 d4 c4 c4 g4 g4 a4 a4 g4 e4 e4 d4 d4 c4";
+
+const underlinedNotes = song.split(" ").map((note) => {
+  const octave = note.charAt(note.length - 1);
+  const color = COLORS[octave];
+  const letterNote = note.slice(0, -1);
+  const underlinedNote = letterNote
+    .split("")
+    .map((c) => `${c}_`)
+    .join("");
+
+  return {
+    note: underlinedNote.slice(0, -1),
+    color,
+  };
+});
+
+function downloadPdf() {
+  const input = document.getElementById("pdf-content");
+  html2canvas(input).then((canvas) => {
+    const imgData = canvas.toDataURL("image/png");
+    const pdf = new jsPDF("l", "pt", [canvas.width, canvas.height]);
+    pdf.addImage(imgData, "PNG", 0, 0);
+    pdf.save("twinkle_twinkle_little_star.pdf");
+  });
+}
+
+function downloadAbc() {
+  const svgString = renderToSvg(song, {});
+  const blob = new Blob([svgString], { type: "image/svg+xml" });
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement("a");
+  link.href = url;
+  link.download = "twinkle_twinkle_little_star.abc";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
 
 function App() {
-  const [abcNotation, setAbcNotation] = useState("");
-  const abcContainerRef = useRef(null);
-
-  useEffect(() => {
-    setAbcNotation(
-      "X:1\nT:Twinkle Twinkle Little Star\nM:4/4\nK:C\nC C C\nG G G\nA A A\nG\nF F F\nE E E\nD D D\nC"
-    );
-  }, []);
-
-  const renderAbc = () => {
-    abcjs.renderAbc(abcContainerRef.current, abcNotation);
-  };
-
-  const handleDownloadPdf = () => {
-    const doc = new PDFDocument();
-    let octave = 0;
-
-    let x = 50;
-    let y = 50;
-
-    abcNotation.split("\n").forEach((note) => {
-      if (note.includes("'")) {
-        octave++;
-      }
-      doc.setTextColor(colors[octave % 8]);
-      doc.text(note, x, y);
-      y += 20;
-    });
-
-    const pdfData = doc.output();
-    const blob = new Blob([pdfData], { type: "application/pdf" });
-    saveAs(blob, "twinkleTwinkleLitleStar.pdf");
-  };
-
   return (
-    <div className="App">
-      <div ref={abcContainerRef} />
-      <button onClick={renderAbc}>Render ABC Notation</button>
-      <button onClick={handleDownloadPdf}>Download ABC Notation (PDF)</button>
+    <div>
+      <div id="pdf-content">
+        <h2>Twinkle, Twinkle, Little Star</h2>
+        {underlinedNotes.map(({ note, color }, i) => (
+          <span key={i} style={{ color, textDecoration: "underline" }}>
+            {note}{" "}
+          </span>
+        ))}
+      </div>
+      <button onClick={downloadPdf}>Download PDF</button>
+      <button onClick={downloadAbc}>Download ABC</button>
     </div>
   );
 }
