5345007d71ceb7664d7accf378cd5ae37a851b7a
dawei-wang <dawei-wang@users.noreply.github.com>
2023-02-16 14:40:05 -0800

    Add Greensleeves color-coded

--- src/App.js
+++ src/App.js
@@ -1,60 +1,56 @@
-import React, { useState, useRef, useEffect } from "react";
-import * as abcjs from "abcjs";
-import PDFDocument from "jspdf";
-import "./App.css";
-import { saveAs } from "file-saver";
+import React from "react";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 
-const colors = [
-  "#ff0000",
-  "#00ff00",
-  "#0000ff",
-  "#ffff00",
-  "#00ffff",
-  "#ff00ff",
-  "#000000",
-  "#ffffff",
-];
+const COLORS = {
+  e: "#ff6666",
+  f: "#ff9966",
+  g: "#ffff66",
+  a: "#99ff99",
+  b: "#66b3ff",
+  c: "#d966ff",
+  d: "#ff66b3",
+};
+
+const song = "e4 d4 c4 d4 e4 e4 e4 g4 g4 g4 g4 e4 e4 e4 e4 d4 d4 e4 d4 c4";
+
+const underlinedNotes = song.split(" ").map((note) => {
+  const octave = note.charAt(note.length - 1);
+  const color = COLORS[note.charAt(0)];
+  const letterNote = note.slice(0, -1);
+  const underlinedNote = letterNote
+    .split("")
+    .map((c) => `${c}_`)
+    .join("");
+
+  return {
+    note: underlinedNote.slice(0, -1),
+    color,
+  };
+});
+
+function downloadPdf() {
+  const input = document.getElementById("pdf-content");
+  html2canvas(input).then((canvas) => {
+    const imgData = canvas.toDataURL("image/png");
+    const pdf = new jsPDF("l", "pt", [canvas.width, canvas.height]);
+    pdf.addImage(imgData, "PNG", 0, 0);
+    pdf.save("greensleeves.pdf");
+  });
+}
 
 function App() {
-  const [abcNotation, setAbcNotation] = useState("");
-  const abcContainerRef = useRef(null);
-
-  useEffect(() => {
-    setAbcNotation(
-      "X:1\nT:Greensleeves\nM:4/4\nK:C\nC C G G\nA7 A7 D D\nE7 E7 A A\nD D G G\nC C G G\nA7 A7 D D\nE7 E7 A A\nD D G G"
-    );
-  }, []);
-
-  const renderAbc = () => {
-    abcjs.renderAbc(abcContainerRef.current, abcNotation);
-  };
-
-  const handleDownloadPdf = () => {
-    const doc = new PDFDocument();
-    let colorIndex = 0;
-
-    let x = 50;
-    let y = 50;
-
-    abcNotation.split("\n").forEach((note) => {
-      if (note.includes("'")) {
-        colorIndex++;
-      }
-      doc.setTextColor(colors[colorIndex % 8]);
-      doc.text(note, x, y);
-      y += 20;
-    });
-
-    const pdfData = doc.output();
-    const blob = new Blob([pdfData], { type: "application/pdf" });
-    saveAs(blob, "greensleeves.pdf");
-  };
-
   return (
-    <div className="App">
-      <div ref={abcContainerRef} />
-      <button onClick={renderAbc}>Render ABC Notation</button>
-      <button onClick={handleDownloadPdf}>Download ABC Notation (PDF)</button>
+    <div>
+      <div id="pdf-content">
+        <h2>Greensleeves</h2>
+        {underlinedNotes.map(({ note, color }, i) => (
+          <span key={i} style={{ color, textDecoration: "underline" }}>
+            {note}{" "}
+          </span>
+        ))}
+      </div>
+      <button onClick={downloadPdf}>Download PDF</button>
     </div>
   );
 }
