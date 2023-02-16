40c1ec9645c0155dab26f4c03e2855cfe77ff1f4
dawei-wang <dawei-wang@users.noreply.github.com>
2023-02-16 14:55:05 -0800

    Add hot cross buns color-coded

--- src/App.js
+++ src/App.js
@@ -1,61 +1,70 @@
-import React, { useState, useRef, useEffect } from "react";
-import * as abcjs from "abcjs";
-import PDFDocument from "jspdf";
-import "./App.css";
-import { saveAs } from "file-saver";
+import React, { useState } from "react";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 
-const colors = [
-  "#ff0000",
-  "#00ff00",
-  "#0000ff",
-  "#ffff00",
-  "#00ffff",
-  "#ff00ff",
-  "#000000",
-  "#ffffff",
-];
+const COLORS = {
+  1: "#ff6666",
+  2: "#ff9966",
+  3: "#ffff66",
+  4: "#99ff99",
+  5: "#66b3ff",
+  6: "#d966ff",
+};
+
+const song = "b4 a4 g4 b4 a4 g4 g4 g4 g4 g4 a4 a4 a4 a4 b4 a4 g4";
+
+const underlinedNotes = song.split(" ").map((note) => {
+  const octave = note.charAt(note.length - 1);
+  const color = COLORS[octave];
+  const letterNote = note.slice(0, -1);
+  const underlinedNote = letterNote
+    .split("")
+    .map((c) => `${c}_`)
+    .join("");
+
+  return {
+    note: underlinedNote.slice(0, -1),
+    color,
+  };
+});
+
+const abcNotation = `X: 1
+T: Hot Cross Buns
+M: 4/4
+L: 1/4
+Q: 1/4=120
+K: G
+${song}`;
+
+function downloadPdf() {
+  const input = document.getElementById("pdf-content");
+  html2canvas(input).then((canvas) => {
+    const imgData = canvas.toDataURL("image/png");
+    const pdf = new jsPDF("l", "pt", [canvas.width, canvas.height]);
+    pdf.addImage(imgData, "PNG", 0, 0);
+    pdf.save("hot_cross_buns.pdf");
+  });
+}
 
 function App() {
-  const [abcNotation, setAbcNotation] = useState("");
-  const abcContainerRef = useRef(null);
-
-  useEffect(() => {
-    setAbcNotation(
-      "X:1\nT:Hot Cross Buns\nM:4/4\nK:C\nC C C\nG G G\nA A A\nG\nF F F\nE E E\nD D D\nC"
-    );
-  }, []);
-
-  const renderAbc = () => {
-    abcjs.renderAbc(abcContainerRef.current, abcNotation);
-  };
-
-  const handleDownloadPdf = () => {
-    const doc = new PDFDocument();
-    let octave = 0;
-
-    let x = 50;
-    let y = 50;
-
-    abcNotation.split("\n").forEach((note) => {
-      doc.fillColor(colors[octave % 8]).text(note, x, y);
-      y += 20;
-
-      if (note.includes("'")) {
-        octave++;
-      }
-    });
-
-    doc.output("dataurlnewwindow");
-    const pdfData = doc.output();
-    const blob = new Blob([pdfData], { type: "application/pdf" });
-    saveAs(blob, "hotCrossBuns.pdf");
-  };
+  const [showAbc, setShowAbc] = useState(false);
 
   return (
-    <div className="App">
-      <div ref={abcContainerRef} />
-      <button onClick={renderAbc}>Render ABC Notation</button>
-      <button onClick={handleDownloadPdf}>Download ABC Notation (PDF)</button>
+    <div>
+      <div id="pdf-content">
+        <h2>Hot Cross Buns</h2>
+        {underlinedNotes.map(({ note, color }, i) => (
+          <span key={i} style={{ color, textDecoration: "underline" }}>
+            {note}{" "}
+          </span>
+        ))}
+      </div>
+      {showAbc ? (
+        <pre>{abcNotation}</pre>
+      ) : (
+        <button onClick={() => setShowAbc(true)}>Show ABC notation</button>
+      )}
+      <button onClick={downloadPdf}>Download PDF</button>
     </div>
   );
 }
