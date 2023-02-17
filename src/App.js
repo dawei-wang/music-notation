b7eccc402845363c0e55a1379b470bafceca4acb
dawei-wang <dawei-wang@users.noreply.github.com>
2023-02-16 18:18:18 -0800

    Add nocturne

--- src/App.js
+++ src/App.js
@@ -3,31 +3,31 @@ import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
 
 const COLORS = {
-  3: "#ffb347",
-  4: "#b19cd9",
-  5: "#ff6961",
-  6: "#77dd77",
-  7: "#aec6cf",
+  2: "#FFB6C1",
+  3: "#F08080",
+  4: "#CD5C5C",
+  5: "#FFA07A",
+  6: "#FA8072",
+  7: "#E9967A",
 };
 
 const song =
-  "g4 g4 a4 g4 c5 b4 g4 g4 a4 g4 d5 c5 g4 g4 g4 g4 a4 b4 c5 c5 b4 a4 g4";
-const notes = song.split(" ");
+  "d3 c3 g3 c4 d4 e4 d4 c4 g3 c4 d4 e4 d4 c4 g3 c4 d4 c4 g3 c4 d4 e4 d4 c4 g3 c4 d4 c4 g3";
 
-const noteGroups = [];
-let currentGroup = [];
-let prevOctave = null;
-for (let i = 0; i < notes.length; i++) {
-  const note = notes[i];
-  const octave = parseInt(note[note.length - 1]);
-  if (prevOctave && octave !== prevOctave) {
-    noteGroups.push(currentGroup);
-    currentGroup = [];
-  }
-  currentGroup.push(note);
-  prevOctave = octave;
-}
-noteGroups.push(currentGroup);
+const underlinedNotes = song.split(" ").map((note) => {
+  const octave = parseInt(note.charAt(note.length - 1));
+  const color = COLORS[octave] || "#000000";
+  const letterNote = note.slice(0, -1);
+  const underlinedNote = letterNote
+    .split("")
+    .map((c) => `${c}_`)
+    .join("");
+
+  return {
+    note: underlinedNote.slice(0, -1),
+    color,
+  };
+});
 
 function downloadPdf() {
   const input = document.getElementById("pdf-content");
@@ -35,30 +35,38 @@ function downloadPdf() {
     const imgData = canvas.toDataURL("image/png");
     const pdf = new jsPDF("l", "pt", [canvas.width, canvas.height]);
     pdf.addImage(imgData, "PNG", 0, 0);
-    pdf.save("circle_of_life.pdf");
+    pdf.save("nocturne.pdf");
   });
 }
 
 function App() {
+  const noteGroups = [];
+  let lastOctave = 0;
+  let group = [];
+  for (let i = 0; i < underlinedNotes.length; i++) {
+    const { note, color } = underlinedNotes[i];
+    const octave = parseInt(note.charAt(note.length - 1));
+    if (octave !== lastOctave) {
+      noteGroups.push(group);
+      group = [{ note, color }];
+      lastOctave = octave;
+    } else {
+      group.push({ note, color });
+    }
+  }
+  noteGroups.push(group);
+
   return (
     <div>
       <div id="pdf-content">
-        <h2>Circle of Life</h2>
+        <h2>Nocturne by Secret Garden</h2>
         {noteGroups.map((group, i) => (
-          <div
-            key={i}
-            style={{ display: "grid", gridTemplateColumns: "repeat(10, 1fr)" }}
-          >
-            {group.map((note, j) => {
-              const octave = parseInt(note[note.length - 1]);
-              const color = COLORS[octave];
-              const letterNote = note.slice(0, -1);
-              return (
-                <span key={j} style={{ color, textDecoration: "underline" }}>
-                  {letterNote}{" "}
-                </span>
-              );
-            })}
+          <div key={i}>
+            {group.map(({ note, color }, j) => (
+              <span key={j} style={{ color, textDecoration: "underline" }}>
+                {note}{" "}
+              </span>
+            ))}
           </div>
         ))}
       </div>
