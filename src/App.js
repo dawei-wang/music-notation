64a3f056408cf9f80313844496e8ba9a39fe64b8
dawei-wang <dawei-wang@users.noreply.github.com>
2023-02-16 17:09:54 -0800

    Add circle of life

--- src/App.js
+++ src/App.js
@@ -3,45 +3,31 @@ import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
 
 const COLORS = {
-  2: "#b7e4c7",
-  3: "#f6d743",
-  4: "#fc913a",
-  5: "#e7552c",
-  6: "#a1241e",
+  3: "#ffb347",
+  4: "#b19cd9",
+  5: "#ff6961",
+  6: "#77dd77",
+  7: "#aec6cf",
 };
 
 const song =
-  "D5 G5 A5 G5 F#5 E5 D5 C#5 D5 F5 E5 D5 C#5 B4 D5 G4 D5 A4 G4 F#4 E4 D4 C#4 D4 F4 E4 D4 C#4 B3 D4 G3 D4 G4 A4 B4 A4 G4 F#4 G4 A4 B4 A4 G4 F#4 E4 D4 E4 F#4 G4 A4 G4 F#4 E4 D4 C#4 D4 E4 F#4 G4 F#4 E4 D4 C#4 B3";
-
+  "g4 g4 a4 g4 c5 b4 g4 g4 a4 g4 d5 c5 g4 g4 g4 g4 a4 b4 c5 c5 b4 a4 g4";
 const notes = song.split(" ");
 
-const createGrid = (notes) => {
-  let currentOctave = null;
-  const grid = [];
-
-  for (let i = 0; i < notes.length; i++) {
-    const note = notes[i];
-    const octave = parseInt(note.charAt(note.length - 1));
-
-    if (octave !== currentOctave) {
-      currentOctave = octave;
-      grid.push([]);
-    }
-
-    const letterNote = note.slice(0, -1);
-    const underlinedNote = letterNote
-      .split("")
-      .map((c) => `${c}_`)
-      .join("");
-
-    grid[grid.length - 1].push({
-      note: underlinedNote.slice(0, -1),
-      color: COLORS[octave],
-    });
+const noteGroups = [];
+let currentGroup = [];
+let prevOctave = null;
+for (let i = 0; i < notes.length; i++) {
+  const note = notes[i];
+  const octave = parseInt(note[note.length - 1]);
+  if (prevOctave && octave !== prevOctave) {
+    noteGroups.push(currentGroup);
+    currentGroup = [];
   }
-
-  return grid;
-};
+  currentGroup.push(note);
+  prevOctave = octave;
+}
+noteGroups.push(currentGroup);
 
 function downloadPdf() {
   const input = document.getElementById("pdf-content");
@@ -49,31 +35,30 @@ function downloadPdf() {
     const imgData = canvas.toDataURL("image/png");
     const pdf = new jsPDF("l", "pt", [canvas.width, canvas.height]);
     pdf.addImage(imgData, "PNG", 0, 0);
-    pdf.save("jupiter.pdf");
+    pdf.save("circle_of_life.pdf");
   });
 }
 
 function App() {
-  const grid = createGrid(notes);
-
   return (
     <div>
       <div id="pdf-content">
-        <h2>Gustav Holst - The Planets: Jupiter</h2>
-        {grid.map((row, i) => (
-          <div key={i} style={{ display: "flex", flexDirection: "row" }}>
-            {row.map(({ note, color }, j) => (
-              <span
-                key={j}
-                style={{
-                  color,
-                  textDecoration: "underline",
-                  marginRight: "5px",
-                }}
-              >
-                {note}{" "}
-              </span>
-            ))}
+        <h2>Circle of Life</h2>
+        {noteGroups.map((group, i) => (
+          <div
+            key={i}
+            style={{ display: "grid", gridTemplateColumns: "repeat(10, 1fr)" }}
+          >
+            {group.map((note, j) => {
+              const octave = parseInt(note[note.length - 1]);
+              const color = COLORS[octave];
+              const letterNote = note.slice(0, -1);
+              return (
+                <span key={j} style={{ color, textDecoration: "underline" }}>
+                  {letterNote}{" "}
+                </span>
+              );
+            })}
           </div>
         ))}
       </div>
