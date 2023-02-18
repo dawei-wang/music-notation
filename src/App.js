0b32b13013d9a07f90f297999cbf26b7bb3a9746
dawei-wang <dawei-wang@users.noreply.github.com>
2023-02-17 16:50:57 -0800

    Add songbook

--- src/App.js
+++ src/App.js
@@ -1,59 +1,121 @@
-import React, { useState, useRef, useEffect } from "react";
-import * as abcjs from "abcjs";
+import React, { useRef } from "react";
+import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
-import "./App.css";
-import FileSaver from "file-saver";
 
-const colors = [
-  "#ff0000",
-  "#00ff00",
-  "#0000ff",
-  "#ffff00",
-  "#00ffff",
-  "#ff00ff",
-  "#000000",
-  "#ffffff",
+const SONGS = [
+  {
+    name: "Imagine",
+    notes:
+      "E4 G4 E5 G5 B5 B4 A4 E5 G5 E5 G5 B5 B4 A4 E5 G5 E5 G5 B5 B4 A4 E5 G5 E5 G5 B5",
+  },
+  {
+    name: "Nocturne",
+    notes:
+      "E4 D#4 E4 D#4 E4 B3 D4 C#4 A3 C4 B3 A3 B3 C4 D4 B3 F#3 G#3 B3 A3 C4 B3 A3",
+  },
+  {
+    name: "Doctor Who Theme",
+    notes:
+      "D4 G4 G4 A4 G4 B4 A4 G4 G4 A4 G4 B4 A4 G4 G4 A4 G4 B4 A4 G4 G4 A4 G4 B4 A4 G4",
+  },
+  {
+    name: "Greensleeves",
+    notes:
+      "E4 D4 E4 F#4 G4 F#4 E4 D4 E4 E4 D4 E4 F#4 G4 F#4 E4 D4 E4 F#4 G4 F#4 E4 D4 E4",
+  },
+  {
+    name: "Hot Cross Buns",
+    notes: "B4 A4 G4 B4 A4 G4 G4 G4 G4 G4 A4 A4 A4 A4 B4 A4 G4 ",
+  },
+  {
+    name: "Twinkle Twinkle Little Star",
+    notes:
+      "C4 C4 G4 G4 A4 A4 G4 F4 F4 E4 E4 D4 D4 C4 G4 G4 F4 F4 E4 E4 D4 G4 G4 F4 F4 E4 E4 D4 C4 C4 G4 G4 A4 A4 G4 F4 F4 E4 E4 D4 D4 C4",
+  },
+  {
+    name: "Circle of Life",
+    notes:
+      "E4 E4 F#4 G#4 G#4 F#4 E4 D4 C#4 A3 A3 C#4 E4 E4 C#4 A3 A3 C#4 E4 E4 F#4 G#4 G#4 F#4 E4 D4 C#4 A3 A3 C#4",
+  },
+  {
+    name: "Jupiter",
+    notes:
+      "C4 C4 C4 C4 C4 C4 C4 C4 E4 E4 E4 E4 E4 E4 E4 E4 G4 G4 G4 G4 G4 G4 G4 G4 E4 E4 E4 E4 E4 E4 E4 E4 C4 C4 C4 C4 C4 C4 C4 C4",
+  },
 ];
 
+const COLORS = {
+  1: "#FFB6C1",
+  2: "#F08080",
+  3: "#CD5C5C",
+  4: "#FFA07A",
+  5: "#FA8072",
+  6: "#E9967A",
+  7: "#BDB76B",
+  8: "#9ACD32",
+  9: "#6B8E23",
+  10: "#556B2F",
+  11: "#228B22",
+  12: "#008000",
+};
+
+function underlineNotes(notes) {
+  return notes.split(" ").map((note, i, arr) => {
+    const octave = parseInt(note.charAt(note.length - 1));
+    const color = COLORS[octave] || "#000000";
+    const letterNote = note.slice(0, -1);
+    const isSharp = letterNote.includes("#");
+    const underlinedNote = isSharp
+      ? letterNote.split("").join("_")
+      : letterNote;
+
+    const isLastNote = i === arr.length - 1;
+    const nextNoteOctave = isLastNote
+      ? null
+      : parseInt(arr[i + 1].charAt(arr[i + 1].length - 1));
+    const shouldAddLineBreak =
+      nextNoteOctave !== null && octave !== nextNoteOctave;
+
+    return {
+      note: underlinedNote,
+      color,
+      shouldAddLineBreak,
+    };
+  });
+}
+
+function downloadPdf() {
+  const input = document.getElementById("pdf-content");
+  html2canvas(input).then((canvas) => {
+    const imgData = canvas.toDataURL("image/png");
+    const pdf = new jsPDF("l", "pt", [canvas.width, canvas.height]);
+    pdf.addImage(imgData, "PNG", 0, 0);
+    pdf.save("songs.pdf");
+  });
+}
+
 function App() {
-  const [abcNotation, setAbcNotation] = useState("");
-  const abcContainerRef = useRef(null);
-
-  useEffect(() => {
-    fetch("sample-tune.json")
-      .then((res) => res.json())
-      .then((data) => {
-        setAbcNotation(data.abcNotation);
-      });
-  }, []);
-
-  const renderAbc = () => {
-    abcjs.renderAbc(abcContainerRef.current, abcNotation);
-  };
-
-  const handleDownloadPdf = () => {
-    const doc = new jsPDF();
-
-    let x = 50;
-    let y = 50;
-    let line = 0;
-
-    abcNotation.split("\n").forEach((note) => {
-      doc.setTextColor(colors[line % 8]);
-      doc.text(note, x, y, { underline: true });
-      y += 20;
-      line++;
-    });
-
-    const pdfBlob = doc.output("blob");
-    FileSaver.saveAs(pdfBlob, "abcNotation.pdf");
-  };
+  const pdfContentRef = useRef(null);
 
   return (
-    <div className="App">
-      <div ref={abcContainerRef} />
-      <button onClick={renderAbc}>Render ABC Notation</button>
-      <button onClick={handleDownloadPdf}>Download ABC Notation (PDF)</button>
+    <div>
+      <div ref={pdfContentRef} id="pdf-content">
+        {SONGS.map(({ name, notes }, i) => (
+          <div key={i}>
+            <h2>{name}</h2>
+            <div style={{ whiteSpace: "nowrap" }}>
+              {underlineNotes(notes).map(
+                ({ note, color, shouldAddLineBreak }, i) => (
+                  <span key={i} style={{ color, textDecoration: "underline" }}>
+                    {note} {shouldAddLineBreak && <br />}
+                  </span>
+                )
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+      <button onClick={downloadPdf}>Download PDF</button>
     </div>
   );
 }
